2aed776234a0e9e6ad842be66fef5f718eb7af4c
Roy <qq3392313023@163.com>
2024-01-04 18:34:40 +0800

    中午定时胡桃酱

--- src/main/java/roy/love/roykook/listener/BotMsg.java
+++ src/main/java/roy/love/roykook/listener/BotMsg.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import love.forte.simbot.ID;
 import love.forte.simbot.Identifies;
 import love.forte.simbot.application.BotManagers;
 import love.forte.simbot.bot.Bot;
@@ -15,12 +16,16 @@ import love.forte.simbot.kook.objects.card.CardElement;
 import love.forte.simbot.kook.objects.card.CardMessageBuilder;
 import love.forte.simbot.kook.objects.card.CardModule;
 import love.forte.simbot.kook.objects.card.Theme;
+import love.forte.simbot.message.Messages;
+import love.forte.simbot.message.MessagesBuilder;
+import love.forte.simbot.resources.Resource;
 import love.forte.simbot.utils.item.Items;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import roy.love.roykook.utils.OK3HttpClient;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -63,6 +68,27 @@ public class BotMsg {
                                         .build()
                         ));
                     });
+                });
+            }
+        }
+    }
+
+    // 每天中午一张MC酱
+    @SneakyThrows
+    @Scheduled(cron = "0 34 18 * * ?")
+    public void sendMCCard() {
+        for (BotManager<?> botManager : botManagers) {
+            Bot bot = botManager.get(Identifies.ID("1K5cGi97SBatmQkd"));
+            Items<Guild> guilds;
+            // api
+            Messages messages = new MessagesBuilder()
+                    .image(Resource.of(new URL("https://api.gumengya.com/Api/McImg?format=image")))
+                    .build();
+            if (bot != null) {
+                guilds = bot.getGuilds();
+                guilds.asStream().forEach(guild -> {
+                    Items<Channel> channels = guild.getChannels();
+                    channels.asStream().forEach(channel -> channel.sendAsync(messages));
                 });
             }
         }
